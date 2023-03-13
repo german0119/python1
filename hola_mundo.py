9a934e4cccb612925000e8c7b902de394a541dde
german0119 <101187591+german0119@users.noreply.github.com>
2023-03-13 15:22:28 -0300

    Add files via upload

---
+++ hola_mundo.py
@@ -0,0 +1,16 @@
+# 1. TAREA: imprimir "Hola mundo" 
+print ( "Hola mundo" )
+# 2. imprimir "¡Hola Noelle!" con el nombre en una variable 
+nombre =  "Noelle" 
+print ( "Hola" , nombre, "!" )	 # con coma 
+print ( "Hola"  + nombre +  "!" )	 # con + 
+# 3. print "Hola 42 !" con el número en una variable 
+nombre =  42 
+print ( "Hola" , 42 , "!"
++  cadena ( 42 ) +  "!" )	 # con un + -- ¡este debería darnos un error! 
+# 4. print "Me encanta comer sushi y pizza". con los alimentos en las variables 
+fave_food1 =  "sushi" 
+fave_food2 =  "pizza" 
+print ( "Me encanta comer {} y {}." . format (fave_food1,fave_food2) ) # with .format() 
+print ( f"Me encanta comer {comida_favorita1} y {comida_favorita2}." ) # con una cadena f
+
